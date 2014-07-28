7cc2a27e0f7fb9c3f2f469e5929792028eae7f2d
Stephen Thoma <sthoma4@gmail.com>
2014-07-28 12:41:29 -0600

    Use current session for auth.

--- app/api/decorators.py
+++ app/api/decorators.py
@@ -1,6 +1,6 @@
 from functools import wraps
 from flask import abort
-from flask import g
+from flask.ext.login import current_user
 from .errors import forbidden
 from ..models import Role
 
@@ -8,8 +8,12 @@ def permission_required(role):
     def decorator(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
-            if not g.current_user.role == role:
-                return forbidden('Insufficient permissions')
+            try:
+                if current_user.role == Role.ADMIN:
+                    pass
+                elif not current_user.role == role:
+                    return forbidden('Insufficient permissions')
+            except: return forbidden('Insufficient permissions')
             return f(*args, **kwargs)
         return decorated_function
     return decorator
