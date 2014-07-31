1c749bdff44724094e409b5790cc1e2e363cbd11
brord van wierst <kwek20@gmail.com>
2014-07-31 22:20:44 +0200

    Blocked autocomplete, field validation done

--- app/dashboard/forms.py
+++ app/dashboard/forms.py
@@ -1,30 +1,21 @@
 from flask.ext.wtf import Form
-from wtforms import PasswordField, BooleanField, SubmitField, ValidationError
+from wtforms import PasswordField, BooleanField, SubmitField
 from wtforms_html5 import EmailField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import InputRequired, Length, Email, EqualTo, Optional
 
-from app.models import User
 
 class SettingsForm(Form):
-    email = EmailField('Change email', validators=[
-        DataRequired(),
-        Length(1, 64),
-        Email()])
-    password = PasswordField('New password', validators=[
-        DataRequired(),
-        Length(8,64, message='Password must be more than 8 characters long.'),
-        EqualTo('password2', message='Passwords must match.')])
-    password2 = PasswordField('Password again', validators=[DataRequired()])
+  email = EmailField('Change email', validators=[Optional(),Email()])
+  password = PasswordField('New password', validators=[Optional(),
+    Length(8, 64, message='Password must be more than 8 characters long.'),
+    EqualTo('password2', message='Passwords must match ')])
+  password2 = PasswordField('Password again', validators=[])
 
-    receiveMail = BooleanField('Receive update mails')
-    option1 = BooleanField('Option 1')
-    option2 = BooleanField('Option 2')
-    option3 = BooleanField('Option 3')
+  receiveMail = BooleanField('Receive update mails')
+  option1 = BooleanField('Option 1')
+  option2 = BooleanField('Option 2')
+  option3 = BooleanField('Option 3')
 
-    passwordConfirm = PasswordField('Password confirmation', validators=[DataRequired()])
-    submit = SubmitField('Update')
-
-    def validate_email(self, field):
-        if User.query.filter_by(email=field.data).first():
-            raise ValidationError('Email already registered.')
+  passwordConfirm = PasswordField('Password confirmation', validators=[InputRequired(message="You must confirm with your current password"), Length(8, 64)])
+  submit = SubmitField('Update')
 
--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,20 +1,36 @@
-from flask import render_template, redirect, url_for, abort, flash, request, \
-                  current_app, make_response
+from flask import render_template, flash
 from flask.ext.login import login_required, current_user
+from wtforms import ValidationError
+
 from . import dashboard
-from .. import db
-from ..models import User, Role
+from ..models import User
 from .forms import SettingsForm
+
 
 @dashboard.route('/', defaults={'path': ''}, methods=['GET', 'POST'])
 @dashboard.route('/<path:path>', methods=['GET', 'POST'])
 @login_required
 def dash(path):
-    form = SettingsForm()
-    if form.validate_on_submit():
-      if not current_user.verify_password(form.passwordConfirm):
-        #entered wrong password
-        flash("Wrong confirmation password")
+  form = SettingsForm()
 
-      flash('submitted')
-    return render_template("dashboard/dashboard_" + current_user.readable_role() + ".html", form=form)
+  if form.validate_on_submit():
+    if current_user.verify_password(form.passwordConfirm.data):
+      if (form.password.data and form.password.data == form.password2.data and not current_user.verify_password(form.password.data)):
+        # change password, not empty and password not same as old
+        flash("new password = " + form.password.data)
+      if form.email.data and not form.email.data == current_user.email:
+        # change email, not empty, not same,
+        if User.query.filter_by(email=form.email.data).first():
+          raise ValidationError('Email already registered.')
+          # email in use
+          flash("email in use")
+        else:
+          flash("email changed to " + form.email.data)
+          # we good
+          # set options
+      flash("Settings updated")
+    else:
+      # entered wrong password
+      flash("Wrong confirmation password")
+
+  return render_template("dashboard/dashboard_" + current_user.readable_role() + ".html", form=form)
