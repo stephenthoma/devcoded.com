bea73358f1fd7c886ae3d5748ba74d8deac82d1d
brord van wierst <kwek20@gmail.com>
2014-07-31 22:28:51 +0200

    password & email change impl done

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,6 +1,8 @@
 from flask import render_template, flash
 from flask.ext.login import login_required, current_user
-from wtforms import ValidationError
+
+from ..email import send_email
+from .. import db
 
 from . import dashboard
 from ..models import User
@@ -17,18 +19,22 @@ def dash(path):
     if current_user.verify_password(form.passwordConfirm.data):
       if (form.password.data and form.password.data == form.password2.data and not current_user.verify_password(form.password.data)):
         # change password, not empty and password not same as old
-        flash("new password = " + form.password.data)
+        current_user.password = form.password.data
+        db.session.add(current_user)
+        flash('Your password has been updated.')
       if form.email.data and not form.email.data == current_user.email:
         # change email, not empty, not same,
         if User.query.filter_by(email=form.email.data).first():
-          raise ValidationError('Email already registered.')
           # email in use
-          flash("email in use")
+          flash('Email already registered.')
         else:
-          flash("email changed to " + form.email.data)
-          # we good
-          # set options
-      flash("Settings updated")
+          new_email = form.email.data
+          token = current_user.generate_email_change_token(new_email)
+          send_email(new_email, 'Confirm your email address',
+                     'auth/mail/change_email',
+                     user=current_user, token=token)
+          flash('An email with instructions to confirm your new email '
+                'address has been sent to you.')
     else:
       # entered wrong password
       flash("Wrong confirmation password")
