595131c6285869caad6801cae24c99ba60a24033
Brord <brordvanwierst@msn.com>
2014-09-10 00:41:07 +0200

    Add plugin assignment functionality

--- app/api/plugins.py
+++ app/api/plugins.py
@@ -6,7 +6,7 @@ from . import api
 from .. import upload
 from .decorators import permission_required
 from .helpers import respond
-from ..models import User, Plugin, PluginUpdate, PluginFile, Role
+from ..models import User, Plugin, PluginUpdate, PluginFile, Role, Status
 
 @api.route('/plugins/')
 def get_all_plugins():
@@ -15,6 +15,30 @@ def get_all_plugins():
     for plugin in plugins:
         json_plugins.append(plugin.to_json())
     return jsonify(respond(200, {'plugins': json_plugins}))
+
+@api.route('/plugins/<int:id>/assign', defaults={'user_id': None})
+@api.route('/plugins/<int:id>/assign/<int:user_id>')
+@permission_required(Role.DEV)
+def assign_plugin(id, user_id):
+    plugin = Plugin.query.get_or_404(id)
+    if current_user.role == Role.ADMIN:
+        plugin.user_id = user_id
+        db.session.commit()
+    elif user_id != None:
+        # Non-Admins cannot assign plugins to others.
+        return jsonify(respond(504))
+    else:
+        user_id = current_user.id # Assigning plugin to self
+        # Get number of plugins that developer is currently working on
+        num_plugins = len(Plugin.query.filter(Plugin.status.in_([1,2]),
+        Plugin.user_id == user_id).all())
+        # Developer isn't working on anything and the plugin isn't assigned
+        if num_plugins < 1 and plugin.user_id is None:
+            plugin.user_id = user_id
+            db.session.commit()
+        else:
+            return jsonify(respond(504))
+    return jsonify(respond(201, {'developer': plugin.user_id}))
 
 @api.route('/plugins/<int:id>')
 def get_plugin(id):
@@ -31,7 +55,7 @@ def new_plugin_update(id):
         db.session.commit()
         return jsonify(respond(201, update.to_json()))
     else:
-        return jsonify(resond(504))
+        return jsonify(respond(504))
 
 @api.route('/plugins/<int:id>/upload/', methods=['POST'])
 @permission_required(Role.DEV)
